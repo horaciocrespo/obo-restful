297f9314f9fe578e2e1980a2f259a75fad7a3079
Horacio Crespo <horacio.crespo@outlook.com>
2021-06-13 20:47:40 -0400

    Adding initial files

---
+++ obo-restful-app/src/main/java/com/obo/oborestfulapp/OboRestfulAppApplication.java
@@ -0,0 +1,13 @@
+package com.obo.oborestfulapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OboRestfulAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OboRestfulAppApplication.class, args);
+    }
+
+}
---
+++ obo-restful-app/src/test/java/com/obo/oborestfulapp/OboRestfulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obo.oborestfulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OboRestfulAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
